1ac26f584197b0edfee3d1c8a742291ce264f4be
furkansimsekli <simseklifurkan0@gmail.com>
2023-08-21 19:03:28 +0300

    config: integrate toml
    Resolves #2

--- src/config.py
+++ src/config.py
@@ -1,5 +1,12 @@
-API_KEY = ''
-DB_STRING = ''
-WEBHOOK_URL = ''
+import sys
 
-PROXY_LIST = []
+import toml
+
+config_path = sys.argv[1] if len(sys.argv) > 1 else 'config.toml'
+config = toml.load(config_path)
+
+API_KEY = config['API_KEY']
+DB_STRING = config['DB_STRING']
+WEBHOOK_URL = config['WEBHOOK_URL']
+
+PROXY_LIST = config['PROXY_LIST']
